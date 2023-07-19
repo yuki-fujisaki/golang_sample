be81827104a12d1af2921751e236d3800c72f3e8
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-07-19 17:15:16 +0900

    [add] サーバー作成

--- cmd/chiserver/main.go
+++ cmd/chiserver/main.go
@@ -13,4 +13,14 @@ type Message struct {
 }
 
 func main() {
+	r := chi.NewRouter()
+
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		msg := Message{Message: "hello"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(msg)
+	})
+
+	fmt.Println("server is running...")
+	http.ListenAndServe(":3000", r)
 }
